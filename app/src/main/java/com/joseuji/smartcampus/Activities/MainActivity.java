98d8bc19983d7c1bef4ae176b0093e42657fd2e5
Víctor García <victor.garciape@gmail.com>
2019-01-04 00:32:34 +0100

    Centrado mapas y más features de rutas

--- app/src/main/java/com/joseuji/smartcampus/Activities/MainActivity.java
+++ app/src/main/java/com/joseuji/smartcampus/Activities/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -26,12 +27,19 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.esri.arcgisruntime.concurrent.ListenableFuture;
+import com.esri.arcgisruntime.data.Feature;
+import com.esri.arcgisruntime.data.FeatureQueryResult;
+import com.esri.arcgisruntime.data.QueryParameters;
+import com.esri.arcgisruntime.data.ServiceFeatureTable;
+import com.esri.arcgisruntime.geometry.Envelope;
 import com.esri.arcgisruntime.geometry.Point;
 import com.esri.arcgisruntime.geometry.Polyline;
+import com.esri.arcgisruntime.layers.FeatureLayer;
 import com.esri.arcgisruntime.loadable.LoadStatus;
 import com.esri.arcgisruntime.location.AndroidLocationDataSource;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.Callout;
 import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener;
 import com.esri.arcgisruntime.mapping.view.Graphic;
 import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
@@ -55,8 +63,14 @@ import com.joseuji.smartcampus.R;
 import com.joseuji.smartcampus.Utils.SmartCampusLayers;
 
 import java.net.MalformedURLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
@@ -81,12 +95,18 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayout linearLayoutFloors;
     ArcGISMap map;
 
+
+
     //Variables para rutas
     private GraphicsOverlay mGraphicsOverlay;
     private Point mStart;
     private Point mEnd;
     private int defaultFloor = 0;   // Route computation needs x, y and z (floor)
     // 1 for trying indoor, to try outdoor change it to 0
+
+    //Variables para Layer Show attributes
+    private Callout mCallout;
+    private ServiceFeatureTable mServiceFeatureTable;
 
     /**************************************************************************************************
      * *                                   ONCREATE()
@@ -212,12 +232,19 @@ public class MainActivity extends AppCompatActivity {
                 //Marca el punto inicial de la ruta, según la ubicación.
 //                setStartMarker(mLocationDisplay.getMapLocation());
 
-                Point busqueda= Consultas.getPuntoBusqueda(retrofitServices, getApplicationContext(), String.valueOf(etSearch.getText()));
 
-                //Marca final de la ruta
-                setEndMarker(busqueda, SimpleMarkerSymbol.Style.DIAMOND, Color.rgb(40, 119, 226), Color.RED);
+                try {
+                    Point  busqueda = Consultas.getPuntoBusqueda(retrofitServices, getApplicationContext(), String.valueOf(etSearch.getText()));
+                    //Marca final de la ruta
+                    setEndMarker(busqueda, SimpleMarkerSymbol.Style.DIAMOND, Color.rgb(40, 119, 226), Color.RED);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    //Se limpian rutas y puntos en el mapa
+                    mEnd=null;
+                    mStart=null;
+                    mGraphicsOverlay.getGraphics().clear();
 
-                //findRoute();//se define la ruta con un trazo
+                }
 
             }
 
@@ -271,7 +298,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
         tgFloor2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -287,7 +313,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
         tgFloor3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -352,6 +377,26 @@ public class MainActivity extends AppCompatActivity {
         // Rutas
         createGraphicsOverlay();
         setupOAuthManager();
+
+        //Mostrar características del mapa
+
+        mCallout=mMapView.getCallout();
+        // get the callout that shows attributes
+        mCallout = mMapView.getCallout();
+        // create the service feature table
+        mServiceFeatureTable = new ServiceFeatureTable(getResources().getString(R.string.sample_service_url));
+        // create the feature layer using the service feature table
+        final FeatureLayer featureLayer = new FeatureLayer(mServiceFeatureTable);
+        // add the layer to the map
+        map.getOperationalLayers().add(featureLayer);
+
+
+
+
+
+
+
+
 
     }
     /**************************************************************************************************
@@ -485,6 +530,7 @@ public class MainActivity extends AppCompatActivity {
         //setMapMarker(location, SimpleMarkerSymbol.Style.SQUARE, Color.rgb(40, 119, 226), Color.RED);
         setMapMarker(location,SimpleMarkerSymbol.Style.DIAMOND, Color.rgb(40, 119, 226), Color.RED);
         mEnd = location;
+        mMapView.setViewpointCenterAsync(location);//Centra el mapa en la ubicación destino
         //findRoute();
     }
 
@@ -495,18 +541,20 @@ public class MainActivity extends AppCompatActivity {
         if (mStart == null) {
             // Start is not set, set it to a tapped location
             setStartMarker(location);
-        }else if (mStart!=null){
+            //mostrarInfo();
+        }else if (mStart!=null && mEnd!=null){
             mGraphicsOverlay.getGraphics().clear();
             setEndMarker(mEnd, SimpleMarkerSymbol.Style.DIAMOND, Color.rgb(40, 119, 226), Color.RED);
             setStartMarker(location);
+
         }
-       /* else if (mEnd == null)
+        else if (mStart!=null && mEnd == null)
         {
-            // End is not set, set it to the tapped location then find the route
-            setEndMarker(location);
-            //findRoute();
-            //mEnd=new Point(Consultas.latitud, Consultas.longitud,Consultas.altitud);
-        }
+            mGraphicsOverlay.getGraphics().clear();
+            setEndMarker(location, SimpleMarkerSymbol.Style.DIAMOND, Color.rgb(40, 119, 226), Color.RED);
+            setStartMarker(location);
+
+        }/*
         else
         {
             // Both locations are set; re-set the start to the tapped location
@@ -514,6 +562,84 @@ public class MainActivity extends AppCompatActivity {
             findRoute();
         }*/
     }
+
+private void mostrarInfo() {
+    // set an on touch listener to listen for click events
+    mMapView.setOnTouchListener(new DefaultMapViewOnTouchListener(this, mMapView) {
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            // remove any existing callouts
+            if (mCallout.isShowing()) {
+                mCallout.dismiss();
+            }
+            // get the point that was clicked and convert it to a point in map coordinates
+            final Point clickPoint = mMapView
+                    .screenToLocation(new android.graphics.Point(Math.round(e.getX()), Math.round(e.getY())));
+            // create a selection tolerance
+            int tolerance = 10;
+            double mapTolerance = tolerance * mMapView.getUnitsPerDensityIndependentPixel();
+            // use tolerance to create an envelope to query
+            Envelope envelope = new Envelope(clickPoint.getX() - mapTolerance, clickPoint.getY() - mapTolerance,
+                    clickPoint.getX() + mapTolerance, clickPoint.getY() + mapTolerance, map.getSpatialReference());
+            QueryParameters query = new QueryParameters();
+            query.setGeometry(envelope);
+            // request all available attribute fields
+            final ListenableFuture<FeatureQueryResult> future = mServiceFeatureTable
+                    .queryFeaturesAsync(query, ServiceFeatureTable.QueryFeatureFields.LOAD_ALL);
+            // add done loading listener to fire when the selection returns
+            future.addDoneListener(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        //call get on the future to get the result
+                        FeatureQueryResult result = future.get();
+                        // create an Iterator
+                        Iterator<Feature> iterator = result.iterator();
+                        // create a TextView to display field values
+                        TextView calloutContent = new TextView(getApplicationContext());
+                        calloutContent.setTextColor(Color.BLACK);
+                        calloutContent.setSingleLine(false);
+                        calloutContent.setVerticalScrollBarEnabled(true);
+                        calloutContent.setScrollBarStyle(View.SCROLLBARS_INSIDE_INSET);
+                        calloutContent.setMovementMethod(new ScrollingMovementMethod());
+                        calloutContent.setLines(5);
+                        // cycle through selections
+                        int counter = 0;
+                        Feature feature;
+                        while (iterator.hasNext()) {
+                            feature = iterator.next();
+                            // create a Map of all available attributes as name value pairs
+                            Map<String, Object> attr = feature.getAttributes();
+                            Set<String> keys = attr.keySet();
+                            for (String key : keys) {
+                                Object value = attr.get(key);
+                                // format observed field value as date
+                                if (value instanceof GregorianCalendar) {
+                                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MMM-yyyy", Locale.US);
+                                    value = simpleDateFormat.format(((GregorianCalendar) value).getTime());
+                                }
+                                // append name value pairs to TextView
+                                calloutContent.append(key + " | " + value + "\n");
+                            }
+                            counter++;
+                            // center the mapview on selected feature
+                            Envelope envelope = feature.getGeometry().getExtent();
+                            mMapView.setViewpointGeometryAsync(envelope, 200);
+                            // show CallOut
+                            mCallout.setLocation(clickPoint);
+                            mCallout.setContent(calloutContent);
+                            mCallout.show();
+                        }
+                    } catch (Exception e) {
+                        Log.e(getResources().getString(R.string.app_name), "Select feature failed: " + e.getMessage());
+                    }
+                }
+            });
+            return super.onSingleTapConfirmed(e);
+        }
+    });
+
+}
 
     public static float getZFromLevel(int level) {
         return level*4 + 4;
